7c13be732f6c748a7e73fb634fa3ea433a53500a
xFariasRDx <101531448+xFariasRDx@users.noreply.github.com>
2022-04-16 22:35:52 -0400

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,47 +1,44 @@
-﻿namespace parte1
+﻿namespace MoneyGram
 {
     class Program
     {
-        static void Main(string [] args)
-        {
+       static void Main(string [] args)
+        {   
+            
+            // START.
             Console.Clear();
+            
             Console.WriteLine(@"
-               ___                                   ,__ __               _                    
-              / (_)                                 /|  |  |             | |    o              
-             |      __   _  _         _   ,_  _|_    |  |  |   __,   __  | |        _  _    _  
-             |     /  \_/ |/ |  |  |_|/  /  |  |     |  |  |  /  |  /    |/ \   |  / |/ |  |/  
-              \___/\__/   |  |_/ \/  |__/   |_/|_/   |  |  |_/\_/|_/\___/|   |_/|_/  |  |_/|__/
-                                                                                                
-                                                                                                                                                                                        
+                ______  ___                                  _________                                     _____ 
+                ___   |/  /______ _______ _____ _____  __    __  ____/______ _______ ___   _______ __________  /_
+                __  /|_/ / _  __ \__  __ \_  _ \__  / / /    _  /     _  __ \__  __ \__ | / /_  _ \__  ___/_  __/
+                _  /  / /  / /_/ /_  / / //  __/_  /_/ /     / /___   / /_/ /_  / / /__ |/ / /  __/_  /    / /_  
+                /_/  /_/   \____/ /_/ /_/ \___/ _\__, /      \____/   \____/ /_/ /_/ _____/  \___/ /_/     \__/  
+                                                /____/                                                           
             ");
-            Console.WriteLine("                                       PRESS ENTER TO START");
-            Console.WriteLine("-                                      --------------------");
+            Console.WriteLine("                                                   PRESS ENTER TO START");
+            Console.WriteLine("                                                   --------------------");
             Console.ReadKey();
             int opcion = 0;
             do
             {
-                Console.Clear();
+                //Console.Clear();
                 Console.WriteLine(@"
-                       MAIN MENU
-                     -------------
 
-                [1]     CELSIUS.
-                [2]     FAHRENHEIT.
-                [3]     KELVIN.
-                [4]     EXIT.
-                
+                                                SELECCIONE LA MONEDA QUE POSEE: 
+                                                ==============================
+                                                
+                                                [1]    (RD$) - PESO.
+                                                [2]    (US$) - DOLLAR.
+                                                [3]    (E$)  - EURO.
+                                                [4]         EXIT.
+                    
                 ");
 
-                
-                Console.WriteLine("            SELECCIONE LA OPCION QUE DESEA: ");
-                Console.WriteLine("            ------------------------------- ");
                 opcion = int.Parse(Console.ReadLine()!);
-
-
                 switch(opcion)
                 {
-                    
-                    //  CONVERSION DE CELSIUS.
+                    //  CONVERSION DE PESO.
                     case 1:
                     {
                         Console.Clear();
@@ -49,9 +46,8 @@
                         A QUE UNIDAD DESEA COMVERTIR?
                         -----------------------------
 
-                            [1] FAHRENHEIT.
-
-                            [2] KELVIN.
+                            [1]    (US$) - DOLLAR.
+                            [2]    (E$)  - EURO.
                         ");
                     
                         int opcion1 = Convert.ToInt32(Console.ReadLine()!);
@@ -59,46 +55,45 @@
                         {
                             case 1:
                             {
-                            Console.Clear();
-                            Console.WriteLine("CONVIRTIENDO DE CELSIUS A FAHRENHEIT.");
-                            Console.WriteLine("-------------------------------------");
-                            Console.WriteLine("INTRODUZA GRADO A CONVERTIR: ");
-                            double num = Convert.ToDouble(Console.ReadLine()!);
-                            // double Resultado = ((num * 9/5) + 32);
-                            Console.Clear();
-                            Console.WriteLine($"{num}°C CONVERTIDOS A FAHRENHEIT ES IGUAL A: {CelsiusToFarenheit(num)} °F");
-                            Console.ReadKey();
+                                Console.Clear();
+                                Console.WriteLine("CONVIRTIENDO A DOLLAR: ");
+                                Console.WriteLine("-----------------------");
+                                Console.WriteLine("INTRODUZCA LA CANTIDAD: ");
+                                int num = Convert.ToInt32(Console.ReadLine()!);
+                                Console.Clear();
+                                Console.WriteLine($"[RD$ {num}] EN DOLLAR ES IGUAL A: [{PesosToDolares(num)} $US]");
+                                Console.ReadKey();
                             }
                             break;
 
                             case 2:
                             {
                                 Console.Clear();
-                                Console.WriteLine("CONVIRTIENDO DE CELSIUS A KELVIN");
-                                Console.WriteLine("--------------------------------");
-                                Console.WriteLine("INTRODUZA GRADO A CONVERTIR: ");
-                                double num = Convert.ToDouble(Console.ReadLine()!);
-                                //double Resultado = (num + 273.15);
+                                Console.WriteLine("CONVIRTIENDO A EURO");
+                                Console.WriteLine("-------------------");
+                                Console.WriteLine("INTRODUZCA LA CANTIDAD: ");
+                                int num = Convert.ToInt32(Console.ReadLine()!);
                                 Console.Clear();
-                                Console.WriteLine($"{num}°C CONVERTIDOS A KELVIN ES IGUAL A: {CelsiusToKelvin(num)} °K");
+                                Console.WriteLine($"[RD$ {num}] EN EURO ES IGUAL A: [{PesosToEuros(num)} EU$]");
                                 Console.ReadKey();
+
                             }
                             break;
-
+                        
                             default:
                             {
-                                Console.Clear();
-                                Console.WriteLine("SELECCION INCORRECTA!");
-                                Console.ReadKey();
+                                Console.WriteLine("RELOADING...");
+                                Console.WriteLine("------------");
+                                Thread.Sleep(3000);
                             }
                             break;
-                            
+                        
                         }
-                    
-                    }break;
-                    
 
-                    //  CONVERSION DE FAHRENHEIT.
+                    }
+                    break;
+
+                    //  CONVERSION DE DOLLAR.
 
                     case 2:
                     {
@@ -106,10 +101,9 @@
                         Console.WriteLine(@"
                         A QUE UNIDAD DESEA COMVERTIR?
                         -----------------------------
-                        
-                            [1] CELSIUS.
 
-                            [2] KELVIN.
+                            [1]    (RD$) - PESO.
+                            [2]    (EU$) - EURO.
                         ");
                     
                         int opcion1 = Convert.ToInt32(Console.ReadLine()!);
@@ -118,44 +112,38 @@
                             case 1:
                             {
                                 Console.Clear();
-                                Console.WriteLine("CONVIRTIENDO DE FAHRENHEIT A CELSIUS.");
-                                Console.WriteLine("-------------------------------------");
-                                Console.WriteLine("INTRODUZA GRADO A CONVERTIR: ");
-                                double num = Convert.ToDouble(Console.ReadLine()!);
-                                double Resultado = ((num - 32) * 5/9);
+                                Console.WriteLine("CONVIRTIENDO A PESO: ");
+                                Console.WriteLine("INTRODUZCA LA CANTIDAD: ");
+                                int num = Convert.ToInt32(Console.ReadLine()!);
                                 Console.Clear();
-                                Console.WriteLine($"{num}°F CONVERTIDOS A CELSIUS ES IGUAL A: {Resultado} °C");
+                                Console.WriteLine($"[US$ {num}] EN PESO ES IGUAL A: [{DollarToPeso(num)} RD$]");
                                 Console.ReadKey();
                             }
                             break;
-                            
+
                             case 2:
                             {
                                 Console.Clear();
-                                Console.WriteLine("CONVIRTIENDO DE FAHRENHEIT A KELVIN.");
-                                Console.WriteLine("------------------------------------");
-                                Console.WriteLine("INTRODUZA GRADO A CONVERTIR: ");
-                                double num = Convert.ToDouble(Console.ReadLine()!);
-                                double Resultado = (((num - 32) * 5/9) + 273.15);
+                                Console.WriteLine("CONVIRTIENDO A EURO");
+                                Console.WriteLine("INTRODUZCA LA CANTIDAD: ");
+                                int num = Convert.ToInt32(Console.ReadLine()!);
                                 Console.Clear();
-                                Console.WriteLine($"{num}°F CONVERTIDOS A KELVIN ES IGUAL A: {Resultado} °K");
+                                Console.WriteLine($"[US$ {num}] EN EURO ES IGUAL A: [{DollarToEuro(num)} EU$]");
                                 Console.ReadKey();
+
                             }
                             break;
-
+                        
                             default:
                             {
-                                Console.Clear();
-                                Console.WriteLine("SELECCION INCORRECTA!");
+                                Console.WriteLine("RELOADING...");
                             }
                             break;
-
-                        }
-                    
+                        }   
                     }
                     break;
 
-                    // CONVERSION DE KELVIN
+                    // CONVERSION DE EURO.
 
                     case 3:
                     {
@@ -164,79 +152,105 @@
                         A QUE UNIDAD DESEA COMVERTIR?
                         -----------------------------
 
-                            [1] CELSIUS.
-                        
-                            [2] FAHRENHEIT.
+                            [1]    (RD$) - PESO.
+                            [2]    (US$) - DOLLAR.
                         ");
-
+                    
                         int opcion1 = Convert.ToInt32(Console.ReadLine()!);
                         switch(opcion1)
                         {
                             case 1:
                             {
                                 Console.Clear();
-                                Console.WriteLine("CONVIRTIENDO DE KELVIN A CELSIUS.");
-                                Console.WriteLine("---------------------------------");
-                                Console.WriteLine("INTRODUZA GRADO A CONVERTIR: ");
-                                double num = Convert.ToDouble(Console.ReadLine()!);
-                                double Resultado = (num - 273.15);
+                                Console.WriteLine("CONVIRTIENDO A PESO: ");
+                                Console.WriteLine("INTRODUZCA LA CANTIDAD: ");
+                                int num = Convert.ToInt32(Console.ReadLine()!);
                                 Console.Clear();
-                                Console.WriteLine($"{num}°K CONVERTIDOS A CELSIUS ES IGUAL A: {Resultado} °C");
-                                Console.ReadKey();
-                            }
-                            break;
-                            
-                            case 2:
-                            {
-                                Console.Clear();
-                                Console.WriteLine("CONVIRTIENDO DE KELVIN A FAHRENHEIT.");
-                                Console.WriteLine("------------------------------------");
-                                Console.WriteLine("INTRODUZA GRADO A CONVERTIR: ");
-                                double num = Convert.ToDouble(Console.ReadLine()!);
-                                double Resultado = (((num - 273.15) * 9/5) + 32);
-                                Console.Clear();
-                                Console.WriteLine($"{num}°C CONVERTIDOS A KELVIN ES IGUAL A: {Resultado} °F");
+                                Console.WriteLine($"[EU$ {num}] EN PESO ES IGUAL A: [{EuroToPeso(num)} RD$]");
                                 Console.ReadKey();
                             }
                             break;
 
-                        }
+                            case 2:
+                            {
+                                Console.Clear();
+                                Console.WriteLine("CONVIRTIENDO A DOLLAR");
+                                Console.WriteLine("INTRODUZCA LA CANTIDAD: ");
+                                int num = Convert.ToInt32(Console.ReadLine()!);
+                                Console.Clear();
+                                Console.WriteLine($"[EU$ {num}] EN DOLLAR ES IGUAL A: [{EuroToDollar(num)} US$]");
+                                Console.ReadKey();
+                            }
+                            break;
+                        
+                            default:
+                            {
+                                Console.WriteLine("RELOADING...");
+                            }
+                            break;
+                        }   
                     }
                     break;
-                    
+
+                    // SALIENDO.
 
                     case 4:
                     {
                         Console.Clear();
-                        Console.WriteLine(@"CERRANDO PROGRAMA...");
+                        Console.WriteLine("SALIENDO");
                         Thread.Sleep(3000);
                     }
                     break;
-                    
+
+                
                     default:
                     {
                         Console.Clear();
-                        Console.WriteLine("PANA USTED VE ESA OPCION?");
+                        Console.WriteLine("USTED VE ESA OPCION?!");
                         Console.ReadKey();
                     }
                     break;
                 }
+                Console.Clear();
             
-            }
-            while(opcion != 4);
-            
+            }while(opcion != 4);
         }
 
-
-        // FUNCIONES.
-        public static double CelsiusToFarenheit(double celsius)
+        //      FUNCIONES.    //
+        public static double PesosToDolares(double pesos)
+        // funcion que convierte de pesos dominicanos a dólares estadounidenses
         {
-            return celsius * 9 / 5 + 32;
+            return pesos * 0.018;
+        } 
+
+        public static double PesosToEuros(double pesos) 
+        {
+          // funcion de convertir de pesos dominicanos a euros  
+          return pesos * 0.017;
+        }
+        public static double DollarToPeso(double dollar)
+        // funcion que convierte de dollar a dominican Bucks.
+        {
+            return dollar * 55.01;
+        }
+    
+        public static double DollarToEuro(double dollar)
+        // funcion que convierte de dollar a dominican Bucks.
+        {
+            return dollar * 0.9172;
+        }
+    
+        public static double EuroToPeso(double Euro)
+        // funcion que convierte de dollar a dominican Bucks.
+        {
+            return Euro * 60.04;
         }
 
-        public static double CelsiusToKelvin(double celsius)
+        public static double EuroToDollar(double Euro)
+        // funcion que convierte de dollar a dominican Bucks.
         {
-            return celsius + 273.15;
+            return Euro * 1.0903;
         }
-    }
+    }   
+
 }
